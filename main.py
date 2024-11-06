f6da4bc6a45db25a03b5198812c4ee049a4df2f2
Azil-rz <649850131@qq.com>
2024-11-06 19:50:50 +0800

    initial AI demo

---
+++ main.py
@@ -0,0 +1,117 @@
+# coding:utf-8
+
+from autogen import Agent
+from autogen import GroupChat, GroupChatManager
+from autogen import AssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+
+model_config_list = [{"model": "deepseek-chat",
+                      "api_key": "sk-c060d2e13ef1487f902735470d24a2c9",
+                      'max_tokens': 4000,
+                      "base_url": "https://api.deepseek.com"}]
+
+retrieve_config_list = [{
+    "task": "default",
+    "docs_path": [
+        "./RAGFiles/FrontEndQuestions.json",
+        "./RAGFiles/ComputerNetworkQuestions.json",
+    ],
+    "chunk_token_size": 2000,
+    "vector_db": "chroma",
+    "overwrite": True,  # set to True if you want to overwrite an existing collection
+    "get_or_create": True,  # set to False if don't want to reuse an existing collection
+}]
+
+ragUserProxyAgent = RetrieveUserProxyAgent(
+    name="interviewee",
+    human_input_mode="ALWAYS",
+    system_message="",
+    max_consecutive_auto_reply=0,
+    retrieve_config=retrieve_config_list[0],
+    code_execution_config=False,  # set to False if you don't want to execute the code
+    description="Interviewee who want to conduct a mock interview."
+)
+
+questionsSelectorAssistantAgent = AssistantAgent(
+    name='questionsSelector',
+    system_message="你是计算机模拟面试系统中的问题收集助手，你的职责是根据面试者想要模拟面试的方向，综合性地选取面试的题目发给面试官。记住，选择的题目数量要有限制，比如前端开发岗位只能选取一题前端相关的题目与一题计算机网络相关的题目。",
+    llm_config={
+        "config_list": model_config_list,
+        "timeout": 600,
+        "cache_seed": 42,
+    },
+    human_input_mode="NEVER",
+    description="Assistant to select questions according to interviewee's request"
+)
+
+interviewerAssistantAgent = AssistantAgent(
+    name='interviewer',
+    system_message="你是计算机模拟面试系统中的问题面试官，你的职责是根据拿到的面试题目与面试者进行对话形式的面试。在每一个问题前需要添加序号，比如问第一个问题时，你要说:第一个问题是。并且在每一问题的结尾要加上：请你回答一下。在面试者回答完问题且回答与答案较为接近或一致时，你需要针对回答中错误的地方给出纠正，然后进行下一个问题的询问。如果与答案差别过大，你可以先不纠正，而是在当前问题上进行诱导回答，最多诱导两轮就要进行下一个问题的询问。每一个问题过后你都要根据面试者的表现进行点评与评分，区间为0至10分，面试者的回答与答案相近给高分，如果不相近则给低分。回答完所有问题后，要将每一题的评分与点评交给最终评分的面试官。",
+    llm_config={"config_list": model_config_list},
+    human_input_mode="NEVER",
+    description="An interviewer who asks the interviewee a list of questions and has a conversation with the interviewee each time he asks a question."
+)
+
+finalSummarizeMakerAssistantAgent = AssistantAgent(
+    name='finalSummarizeMaker',
+    system_message="你是计算机模拟面试系统中的最终评分面试官。你的职责是根据问题面试官发来的面试者的面试表现与评分进行总结，并根据综合评分与表现来判定此次面试是否通过，将综合点评结果与通过与否告诉面试者。",
+    llm_config={"config_list": model_config_list},
+    human_input_mode="NEVER",
+    description="The final interviewer summarizes the interviewee's scores and comments based on the interviewer's scores and comments"
+)
+
+
+def reset_agents():
+    questionsSelectorAssistantAgent.reset()
+    interviewerAssistantAgent.reset()
+    finalSummarizeMakerAssistantAgent.reset()
+
+
+def next_speaker_selection_func(last_speaker: Agent, simulateGroupChat: GroupChat):
+    messages = simulateGroupChat.messages
+
+    if len(messages) < 1:
+        return ragUserProxyAgent
+
+    elif len(messages) == 1:
+        return questionsSelectorAssistantAgent
+
+    elif last_speaker is questionsSelectorAssistantAgent:
+        return interviewerAssistantAgent
+
+    elif last_speaker is interviewerAssistantAgent:
+        if "请你回答" in messages[-1]["content"]:
+            return ragUserProxyAgent
+        else:
+            return finalSummarizeMakerAssistantAgent
+
+    elif last_speaker is ragUserProxyAgent:
+        return interviewerAssistantAgent
+
+    elif last_speaker is finalSummarizeMakerAssistantAgent:
+        return ragUserProxyAgent
+
+    else:
+        return "auto"
+
+
+def initial_chat():
+    reset_agents()
+    simulateGroupChat = GroupChat(
+        agents=[ragUserProxyAgent, questionsSelectorAssistantAgent, interviewerAssistantAgent,
+                finalSummarizeMakerAssistantAgent],
+        messages=[],
+        max_round=20,
+        speaker_selection_method=next_speaker_selection_func,
+        send_introductions=True
+    )
+    manager = GroupChatManager(groupchat=simulateGroupChat, llm_config={"config_list": model_config_list})
+
+    ragUserProxyAgent.initiate_chat(
+        manager,
+        message=ragUserProxyAgent.message_generator,
+        problem="你好，我是这次的面试者，想进行前端方向的模拟面试。"
+    )
+
+
+initial_chat()
